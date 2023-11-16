8aece49fd53181139c6298b1b7bfda7c14edb67c
ByungChan Lee <libc@furiosa.ai>
2023-11-16 16:01:39 +0900

    add result

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,14 @@ fn main() {
     let _guard = chrometracer::builder().init();
     foo();
 }
+
+/*
+ * result sample
+
+[
+{"name":"bar","ts":103.101,"dur":10.502,"pid":602505,"tid":1,"ph":"X"},
+{"name":"bar","ts":122.241,"dur":2.866,"pid":602505,"tid":1,"ph":"X"},
+{"name":"foo","ts":102.968,"dur":22.339,"pid":602505,"tid":1,"ph":"X"}
+]
+ 
+ */
